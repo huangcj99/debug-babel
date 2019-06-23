b38d19921d15bd75661dc0950b9f677f3e13aec5
黄诚就 <huangcj@iaskdoc.com>
2019-06-23 17:41:09 +0800

    添加示例

--- index.mjs
+++ index.mjs
@@ -1,8 +1,50 @@
 import fs from 'fs-extra'
 import path from 'path'
-import * as babylon from "babylon"
+import babylon from "babylon"
+import traverse from "babel-traverse"
+import generate from 'babel-generator'
+import t from "babel-types"
 
 const codePath = path.resolve(process.cwd(), './code.js')
-const code = fs.readFileSync(codePath)
+const ogenCodePath = path.resolve(process.cwd(), './gen-code.js')
+const code = fs.readFileSync(codePath).toString()
 
-console.log(code.toString())
+const ast = babylon.parse(code, {
+  sourceType: "module",
+  plugins: []
+})
+
+traverse.default(ast, {
+  ImportDeclaration(path) {
+    const specifiers = path.node.specifiers
+    const source = path.node.source
+
+    if (source.value === 'react') {
+      let propTypeNode = null
+
+      // 剔除PropTypes
+      path.node.specifiers = specifiers.filter((specifier, i) => {
+        if (specifier.local.name === 'PropTypes') {
+          // 创建新的PropTypes节点
+          propTypeNode = t.ImportDeclaration(
+            [
+              t.importDefaultSpecifier(specifier.local)
+            ],
+            t.StringLiteral('prop-types')
+          )
+        } else {
+          return specifier
+        }
+      })
+
+      path.insertAfter(propTypeNode)
+    }
+  }
+})
+
+fs.outputFileSync(ogenCodePath, generate.default(ast, {}, code).code)
+
+
+
+
+
